01dec7b18d6189d661206df1b9711caf34dd6722
Kuki Amarilla <sc.amarilla@gmail.com>
2021-07-11 18:46:38 -0400

    Agregado pep8, flake8, coverage y test de ejemplo

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,6 +6,9 @@ class Message(models.Model):
     subject = models.CharField(max_length=200)
     body = models.TextField()
 
+    def __str__(self):
+        return self.subject + ": " + self.body
+
 
 class MessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
