e8fddd631ddf0e95f7e8884b6bc80c58c61a6e87
Kuki Amarilla <sc.amarilla@gmail.com>
2021-07-12 01:09:19 -0400

    Agregado soporte para tests e2e con cypress

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-vue-template',
+        'USER': 'django-vue-template',
+        'PASSWORD': 'django-vue-template',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
